fca4966450c8f042d5f5cc6de961b61b23959b86
Linda Mbongue <linda.mbongue@outlook.fr>
2026-04-25 09:24:44 +0000

    Make the robot complete one clockwise and one counter clockwise lap

--- robot_patrol/include/robot_patrol/patrol.h
+++ robot_patrol/include/robot_patrol/patrol.h
@@ -1,9 +1,12 @@
+#pragma once
+
 #include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp/timer.hpp"
 #include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
 #include <geometry_msgs/msg/twist.hpp>
-#include <rclcpp/qos.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <string>
@@ -21,14 +24,23 @@ private:
   std::string node_name_;
   float direction_;
   bool obstacle_detected_ = false;
+  bool first_odom_ = false;
+  double previous_yam_;
+  double accumulated_yaw_;
+  bool lap_completed_ = false;
+  bool turn_around_completed_ = false;
+
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laserscan_sub_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
   rclcpp::TimerBase::SharedPtr cmd_vel_pub_timer_;
 
+  void odom_callback_(const nav_msgs::msg::Odometry::SharedPtr msg);
   void laserscan_callback_(const sensor_msgs::msg::LaserScan::SharedPtr msg);
   bool is_obstacle_detected_(const sensor_msgs::msg::LaserScan::SharedPtr msg);
   void identify_safest_direction_to_move_next(
       const sensor_msgs::msg::LaserScan::SharedPtr msg);
   void move_robot_forward_();
+  void turn_robot_around_();
   void cmd_vel_pub_timer_clbk_();
 };
--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,5 +1,5 @@
 #include "robot_patrol/patrol.h"
-#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
 #include "rclcpp/logging.hpp"
 #include "rclcpp/qos.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
@@ -7,8 +7,11 @@
 #include <chrono>
 #include <cmath>
 #include <csignal>
+#include <functional>
 #include <math.h>
 #include <memory>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 Patrol::Patrol(const std::string &node_name)
     : Node(node_name), node_name_(node_name) {
@@ -19,6 +22,10 @@ Patrol::Patrol(const std::string &node_name)
       "/fastbot_1/scan", qos,
       std::bind(&Patrol::laserscan_callback_, this, std::placeholders::_1));
 
+  this->odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/fastbot_1/odom", qos,
+      std::bind(&Patrol::odom_callback_, this, std::placeholders::_1));
+
   this->cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
       "/fastbot_1/cmd_vel", 10);
 
@@ -26,6 +33,54 @@ Patrol::Patrol(const std::string &node_name)
 
   this->cmd_vel_pub_timer_ = this->create_wall_timer(
       timer_period, std::bind(&Patrol::cmd_vel_pub_timer_clbk_, this));
+}
+
+void Patrol::odom_callback_(const nav_msgs::msg::Odometry::SharedPtr msg) {
+
+  // Retrieve robot orientation from msg
+  tf2::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
+                    msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+
+  // Convert quaternion to angle (roll, pitch, yaw)
+  // with yaw -> orientation around the z-axis
+  tf2::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  // For the first odom msg, just init previous_yam_
+  if (first_odom_) {
+    this->previous_yam_ = yaw;
+    this->first_odom_ = false;
+    return;
+  }
+
+  // Calculate the variation angle around the z-axis
+  double delta_yaw = yaw - this->previous_yam_;
+
+  // Normalize the delta_yaw to the range [-pi, pi]
+  delta_yaw = std::fmod(delta_yaw + M_PI, 2 * M_PI) - M_PI;
+
+  // Calculate the total robot rotation done since the robot is moving
+  this->accumulated_yaw_ += std::abs(delta_yaw);
+  RCLCPP_INFO(this->get_logger(), "accumulated_yaw_ : %.2f",
+              std::abs(this->accumulated_yaw_));
+
+  // Update previous_yaw for the next calculation
+  this->previous_yam_ = yaw;
+
+  // If the robot has completed a full lap
+  if (std::abs(this->accumulated_yaw_) >= 2.0 * M_PI &&
+      !(this->lap_completed_)) {
+
+    RCLCPP_INFO(this->get_logger(), "1 full lap completed !");
+    this->lap_completed_ = true;
+
+    // Prepare robot to turn around after a lap completed
+    this->turn_around_completed_ = false;
+
+    // Prepare robot to calculate accumulated_yaw_ for the 180 deg rotation
+    this->accumulated_yaw_ = 0.0;
+  }
 }
 
 void Patrol::laserscan_callback_(
@@ -75,9 +130,9 @@ void Patrol::identify_safest_direction_to_move_next(
   this->direction_ =
       msg->angle_min + (max_length_index * msg->angle_increment); // in rads
 
-  RCLCPP_INFO(this->get_logger(),
-              "max_length: %.2f | max_length_index: %d | direction_ = %.2f",
-              max_length, max_length_index, this->direction_);
+  // RCLCPP_INFO(this->get_logger(),
+  //"max_length: %.2f | max_length_index: %d | direction_ = %.2f",
+  // max_length, max_length_index, this->direction_);
 }
 
 void Patrol::move_robot_forward_() {
@@ -86,6 +141,33 @@ void Patrol::move_robot_forward_() {
   move_forward_msg.linear.x = 0.1;
   move_forward_msg.angular.z = 0.0;
   this->cmd_vel_pub_->publish(move_forward_msg);
+}
+
+void Patrol::turn_robot_around_() {
+
+  auto turn_around_msg = geometry_msgs::msg::Twist();
+
+  // If the robot has not finish to turn around
+  if (std::abs(this->accumulated_yaw_) < M_PI) {
+
+    RCLCPP_INFO(this->get_logger(), "I'm turning around...");
+    turn_around_msg.angular.z = this->direction_ / 2; // Continue to rotate
+
+  } else {
+
+    RCLCPP_INFO(this->get_logger(), "Turn around completed !");
+    turn_around_msg.angular.z = 0.0; // Stop rotating
+
+    this->turn_around_completed_ = true;
+
+    // Prepare robot to deetct if a new lap is completed
+    this->lap_completed_ = false;
+
+    // Prepare robot to calculate accumulated_yaw_ for the full lap
+    this->accumulated_yaw_ = 0.0;
+  }
+
+  this->cmd_vel_pub_->publish(turn_around_msg);
 }
 
 void Patrol::stop_robot() {
@@ -108,8 +190,8 @@ bool Patrol::is_obstacle_detected_(
     // If the ray length is different from inf, -inf and NAN AND is < 35cm
     if (std::isfinite(msg->ranges[i]) && msg->ranges[i] < 0.35) {
 
-      RCLCPP_INFO(this->get_logger(), "Obstacle detected at %.2f m ! ",
-                  msg->ranges[i]);
+      // RCLCPP_INFO(this->get_logger(), "Obstacle detected at %.2f m ! ",
+      // msg->ranges[i]);
       return true;
 
     } else {
@@ -122,7 +204,9 @@ bool Patrol::is_obstacle_detected_(
 
 void Patrol::cmd_vel_pub_timer_clbk_() {
 
+  // If obstacle detected AND robot is not turning around
   if (this->obstacle_detected_) {
+
     // Rotate to avoid obstacle
     auto avoid_msg = geometry_msgs::msg::Twist();
     avoid_msg.linear.x = 0.1;
@@ -130,8 +214,17 @@ void Patrol::cmd_vel_pub_timer_clbk_() {
     this->cmd_vel_pub_->publish(avoid_msg);
 
   } else {
+
     RCLCPP_INFO(this->get_logger(), "No obstacle in front of me.");
-    move_robot_forward_();
+
+    if (this->lap_completed_ && !(this->turn_around_completed_)) {
+
+      turn_robot_around_();
+
+    } else {
+
+      move_robot_forward_();
+    }
   }
 }
 
