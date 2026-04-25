2080d0cbb82a022a619a846018baedf1c3b24d85
Linda Mbongue <linda.mbongue@outlook.fr>
2026-04-25 01:27:09 +0000

    Rviz node added to the launch file

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,12 +1,39 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description() :
     
-    return LaunchDescription([
-    Node(
+    # Get Package Description and Directory #
+    package_description = "robot_patrol"
+    package_directory = get_package_share_directory(package_description)
+
+    # Robot Patrol # -> To start robot patrolling
+    robot_patrol_node = Node(
         package="robot_patrol",
         executable="robot_patrol_node",
         output="screen",
-        emulate_tty=True)
+        emulate_tty=True
+    )
+
+    # Load RViz Configuration File #
+    rviz_config_file = "config.rviz"
+    rviz_config_path = os.path.join(package_directory, "rviz", rviz_config_file)
+    print("RViz Config Loaded !")
+
+    # Rviz2 Launch Configuration (RViz)#
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        robot_patrol_node,
+        rviz2_node
     ])
