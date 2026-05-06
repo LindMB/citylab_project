b7001f72eb39980a2729ed43e11e86ca2d4ceef8
Linda Mbongue <linda.mbongue@outlook.fr>
2026-05-06 19:10:36 +0000

    Robot patrolling with the direction service implemented

---
+++ robot_patrol/include/robot_patrol/patrol_with_service.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "geometry_msgs/msg/detail/twist__struct.hpp"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
+#include "rclcpp/client.hpp"
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/timer.hpp"
+#include "robot_patrol/srv/get_direction.hpp"
+#include "sensor_msgs/msg/detail/laser_scan__struct.hpp"
+#include <geometry_msgs/msg/twist.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <string>
+
+class Patrol : public rclcpp::Node {
+
+public:
+  Patrol(const std::string &node_name);
+
+  void stop_robot();
+
+  ~Patrol() = default;
+
+private:
+  std::string node_name_;
+
+  bool first_odom_ = false;
+  bool start_position_initialized_ = false;
+
+  bool obstacle_detected_on_the_left_ = false;
+  bool obstacle_detected_on_the_right_ = false;
+  bool lap_completed_ = false;
+  bool turn_around_completed_ = false;
+
+  double direction_; // angular z velocity
+  double previous_yam_;
+  double accumulated_turn_yaw_;
+
+  double start_pos_x_ = 0.0;
+  double start_pos_y_ = 0.0;
+
+  double current_pos_x_ = 0.0;
+  double current_pos_y_ = 0.0;
+
+  double previous_pos_x_ = 0.0;
+  double previous_pos_y_ = 0.0;
+
+  double traveled_distance_ = 0.0;
+
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laserscan_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+  rclcpp::TimerBase::SharedPtr cmd_vel_pub_timer_;
+  rclcpp::Client<robot_patrol::srv::GetDirection>::SharedPtr
+      direction_service_client_;
+
+  void odom_callback_(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void laserscan_callback_(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  bool is_obstacle_detected_(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void move_robot_forward_();
+  void turn_robot_around_();
+  void avoid_obstacle_();
+  void cmd_vel_pub_timer_clbk_();
+  void handle_service_response_(
+      const rclcpp::Client<robot_patrol::srv::GetDirection>::SharedFuture
+          result_future);
+};
---
+++ robot_patrol/src/patrol_with_service.cpp
@@ -0,0 +1,355 @@
+#include "robot_patrol/patrol_with_service.h"
+#include "nav_msgs/msg/detail/odometry__struct.hpp"
+#include "rclcpp/client.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/qos.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <csignal>
+#include <functional>
+#include <math.h>
+#include <memory>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+Patrol::Patrol(const std::string &node_name)
+    : Node(node_name), node_name_(node_name) {
+
+  auto qos = rclcpp::QoS(10).reliability(rclcpp::ReliabilityPolicy::Reliable);
+
+  this->laserscan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "/fastbot_1/scan", qos,
+      std::bind(&Patrol::laserscan_callback_, this, std::placeholders::_1));
+
+  this->odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/fastbot_1/odom", qos,
+      std::bind(&Patrol::odom_callback_, this, std::placeholders::_1));
+
+  this->cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+      "/fastbot_1/cmd_vel", 10);
+
+  auto timer_period = std::chrono::milliseconds(100); // 10Hz = 0.1s = 100ms
+
+  this->cmd_vel_pub_timer_ = this->create_wall_timer(
+      timer_period, std::bind(&Patrol::cmd_vel_pub_timer_clbk_, this));
+
+  const std::string service_name = "/direction_service";
+  this->direction_service_client_ =
+      this->create_client<robot_patrol::srv::GetDirection>(service_name);
+
+  // Wait for the service to be available (checks every second)
+  while (
+      !direction_service_client_->wait_for_service(std::chrono::seconds(1))) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(this->get_logger(),
+                   "Interrupted while waiting for the service. Exiting.");
+      return;
+    }
+
+    RCLCPP_INFO(this->get_logger(),
+                "Service %s not available, waiting again...",
+                service_name.c_str());
+  }
+}
+
+void Patrol::odom_callback_(const nav_msgs::msg::Odometry::SharedPtr msg) {
+
+  // Retrieve robot position from msg
+  this->current_pos_x_ = msg->pose.pose.position.x;
+  this->current_pos_y_ = msg->pose.pose.position.y;
+
+  // Retrieve robot orientation from msg
+  tf2::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y,
+                    msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+
+  // Init the robot starting position
+  if (!this->start_position_initialized_) {
+    this->start_pos_x_ = this->current_pos_x_;
+    this->start_pos_y_ = this->current_pos_y_;
+
+    this->previous_pos_x_ = this->current_pos_x_;
+    this->previous_pos_y_ = this->current_pos_y_;
+
+    this->start_position_initialized_ = true;
+  }
+
+  // Convert quaternion to angle (roll, pitch, yaw)
+  // with yaw -> orientation around the z-axis
+  tf2::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  // For the first odom msg, just init previous_yam_
+  if (first_odom_) {
+    this->previous_yam_ = yaw;
+    this->first_odom_ = false;
+    return;
+  }
+
+  // Calculate the change in position between the current and previous positions
+  double dx = this->current_pos_x_ - this->previous_pos_x_;
+  double dy = this->current_pos_y_ - this->previous_pos_y_;
+
+  // Calculate the total distance traveled since the robot is moving
+  this->traveled_distance_ += std::sqrt(dx * dx + dy * dy);
+
+  RCLCPP_INFO(this->get_logger(), "traveled_distance_ : %.2f",
+              this->traveled_distance_);
+
+  // Update the previous robot position
+  this->previous_pos_x_ = this->current_pos_x_;
+  this->previous_pos_y_ = this->current_pos_y_;
+
+  // Calculate the variation angle around the z-axis
+  double delta_yaw = yaw - this->previous_yam_;
+
+  // Normalize the delta_yaw to the range [-pi, pi]
+  // delta_yaw = std::fmod(delta_yaw + M_PI, 2 * M_PI) - M_PI;
+
+  if (delta_yaw > M_PI) {
+    delta_yaw -= 2.0 * M_PI;
+  } else if (delta_yaw < -M_PI) {
+    delta_yaw += 2.0 * M_PI;
+  }
+
+  // Robot is currently doing the 180 deg turn
+  if (this->lap_completed_ && !this->turn_around_completed_) {
+
+    this->accumulated_turn_yaw_ += std::abs(delta_yaw);
+
+    RCLCPP_INFO(this->get_logger(), "accumulated_turn_yaw_ : %.2f",
+                this->accumulated_turn_yaw_);
+  }
+
+  // Update previous_yaw for the next calculation
+  this->previous_yam_ = yaw;
+
+  // Calculate the change in position between the current and starting positions
+  double dx_start = this->current_pos_x_ - this->start_pos_x_;
+  double dy_start = this->current_pos_y_ - this->start_pos_y_;
+
+  // Calculate the distance from where the robot is currently
+  // to where its starting point is
+  double distance_from_start =
+      std::sqrt(dx_start * dx_start + dy_start * dy_start);
+
+  RCLCPP_INFO(this->get_logger(), "distance_from_start : %.2f",
+              distance_from_start);
+
+  // If the distance between the robot and its starting point is less than 40cm
+  // if the robot traveled more than 2.0 meters
+  if (distance_from_start < 0.40 && this->traveled_distance_ > 2.0 &&
+      !this->lap_completed_) {
+
+    RCLCPP_INFO(this->get_logger(), "1 full lap completed !");
+
+    this->lap_completed_ = true;
+
+    // Prepare robot to turn around after a lap completed
+    this->turn_around_completed_ = false;
+    this->accumulated_turn_yaw_ = 0.0;
+  }
+}
+
+void Patrol::laserscan_callback_(
+    const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+  /*RCLCPP_INFO(this->get_logger(),
+              "angle_min: %.2f | angle_max: %.2f | angle_increment = %.2f",
+              msg->angle_min, msg->angle_max, msg->angle_increment);*/
+
+  if (is_obstacle_detected_(msg)) {
+
+    auto request = std::make_shared<robot_patrol::srv::GetDirection::Request>();
+    // Fill request with msg
+    request->laser_data = *msg;
+
+    if (!direction_service_client_->service_is_ready()) {
+      RCLCPP_WARN(this->get_logger(), "Failed to call service");
+      return;
+    }
+
+    // Send the request asynchronously
+    // and when a response is received, use the handle_service_response_ fct
+    auto result_future = this->direction_service_client_->async_send_request(
+        request, std::bind(&Patrol::handle_service_response_, this,
+                           std::placeholders::_1));
+  } else {
+    // Do nothing
+  }
+}
+
+void Patrol::handle_service_response_(
+    const rclcpp::Client<robot_patrol::srv::GetDirection>::SharedFuture
+        result_future) {
+
+  auto response = result_future.get();
+
+  if (response->direction == "right") {
+    this->direction_ = -0.5;
+  } else if (response->direction == "left") {
+    this->direction_ = 0.5;
+  } else { // response->direction == "forward"
+    this->direction_ = 0.0;
+  }
+}
+
+void Patrol::move_robot_forward_() {
+
+  auto move_forward_msg = geometry_msgs::msg::Twist();
+  move_forward_msg.linear.x = 0.1;
+  move_forward_msg.angular.z = 0.0;
+  this->cmd_vel_pub_->publish(move_forward_msg);
+}
+
+void Patrol::turn_robot_around_() {
+
+  auto turn_around_msg = geometry_msgs::msg::Twist();
+
+  // If the robot has not finish to turn around
+  if (std::abs(this->accumulated_turn_yaw_) < M_PI) {
+
+    RCLCPP_INFO(this->get_logger(), "I'm turning around...");
+    turn_around_msg.angular.z = this->direction_ / 2; // Continue to rotate
+
+  } else {
+
+    RCLCPP_INFO(this->get_logger(), "Turn around completed !");
+    turn_around_msg.angular.z = 0.0; // Stop rotating
+
+    this->turn_around_completed_ = true;
+
+    // Prepare robot to detect if a new lap is completed
+    this->lap_completed_ = false;
+
+    // Prepare robot to calculate accumulated_turn_yaw_ for the full lap
+    this->accumulated_turn_yaw_ = 0.0;
+
+    // Reset all positions for the next lap
+    this->start_pos_x_ = this->current_pos_x_;
+    this->start_pos_y_ = this->current_pos_y_;
+
+    this->previous_pos_x_ = this->current_pos_x_;
+    this->previous_pos_y_ = this->current_pos_y_;
+
+    // Reset the robot traveled distance for the next lap
+    this->traveled_distance_ = 0.0;
+  }
+
+  this->cmd_vel_pub_->publish(turn_around_msg);
+}
+
+void Patrol::avoid_obstacle_() {
+  auto avoid_msg = geometry_msgs::msg::Twist();
+  avoid_msg.linear.x = 0.1;
+  avoid_msg.angular.z = this->direction_;
+  this->cmd_vel_pub_->publish(avoid_msg);
+}
+
+void Patrol::stop_robot() {
+
+  auto stop_msg = geometry_msgs::msg::Twist(); // All fields default to zero
+  this->cmd_vel_pub_->publish(stop_msg);
+  RCLCPP_INFO(this->get_logger(), "Publishing stop message before shutdown");
+}
+
+bool Patrol::is_obstacle_detected_(
+    const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+
+  // Re init obstacle detection at each laserscan msg reading
+  // to check where the obstacle is in comparison to the robot
+  this->obstacle_detected_on_the_left_ = false;
+  this->obstacle_detected_on_the_right_ = false;
+
+  double angle;
+
+  for (int i = 0; i < (int)msg->ranges.size(); i++) {
+
+    angle = msg->angle_min + (i * msg->angle_increment);
+
+    // Normalize angle
+    // from [0, 2pi] (lidar in real life) to [-pi, pi] (lidar in simulation)
+    if (angle > M_PI) {
+      angle -= 2.0 * M_PI;
+    }
+
+    // If the ray length is different from inf, -inf and NAN AND is < 35cm
+    if (std::isfinite(msg->ranges[i]) && msg->ranges[i] < 0.35) {
+
+      // For a ray of the very front section (between -10deg and 10deg) of the
+      // lidar...
+      if (angle >= -(M_PI / 18) && angle <= (M_PI / 18)) {
+        RCLCPP_INFO(this->get_logger(),
+                    "Obstacle detected at %.2f m on the very front !",
+                    msg->ranges[i]);
+        // Both flags at true to allow to look for the safest direction
+        // between [0, pi] and [-pi/2, 0]
+        this->obstacle_detected_on_the_left_ = true;
+        this->obstacle_detected_on_the_right_ = true;
+      }
+
+      // For a ray of the front-left section (between 15deg and 30deg) of the
+      // lidar...
+      if (angle >= M_PI / 12 && angle <= M_PI / 6) {
+        RCLCPP_INFO(this->get_logger(),
+                    "Obstacle detected at %.2f m on the left ! ",
+                    msg->ranges[i]);
+        this->obstacle_detected_on_the_left_ = true;
+      }
+
+      // For a ray of the front-right section (between -15deg and -30deg) of the
+      // lidar...
+      else if (angle >= -M_PI / 6 && angle <= -M_PI / 12) {
+        RCLCPP_INFO(this->get_logger(),
+                    "Obstacle detected at %.2f m on the right !",
+                    msg->ranges[i]);
+        this->obstacle_detected_on_the_right_ = true;
+      }
+    }
+  }
+
+  // Return true if at least one of them is true
+  return this->obstacle_detected_on_the_left_ ||
+         this->obstacle_detected_on_the_right_;
+}
+
+void Patrol::cmd_vel_pub_timer_clbk_() {
+
+  // When a lap has been completed, do a 180deg turn
+  // and when turning around, ignore obstacle detection
+  if (this->lap_completed_ && !(this->turn_around_completed_)) {
+
+    turn_robot_around_();
+  }
+  // If obstacle detected
+  else if (this->obstacle_detected_on_the_left_ ||
+           this->obstacle_detected_on_the_right_) {
+
+    avoid_obstacle_();
+
+  } else {
+    move_robot_forward_();
+  }
+}
+
+std::shared_ptr<Patrol> robot_patrol_node;
+
+void signal_handler(int /*signum*/) { // intentionally unused
+  robot_patrol_node->stop_robot();
+  rclcpp::shutdown();
+}
+
+int main(int argc, char **argv) {
+
+  rclcpp::init(argc, argv);
+  robot_patrol_node =
+      std::make_shared<Patrol>("robot_patrol_with_service_node");
+
+  // Register the signal handler for CTRL+C
+  std::signal(SIGINT, signal_handler);
+
+  rclcpp::spin(robot_patrol_node);
+  return 0;
+}
