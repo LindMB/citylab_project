c39aa050cba78fa7a792368c526b8b41d3908f03
Linda Mbongue <linda.mbongue@outlook.fr>
2026-05-09 15:51:04 +0000

    Launched file added to start the go to pose action server

---
+++ robot_patrol/launch/start_gotopose_action.launch.py
@@ -0,0 +1,40 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description() :
+
+    # Get Package Description and Directory #
+    package_description = "robot_patrol"
+    package_directory = get_package_share_directory(package_description)
+
+
+    # /go_to_pose Service Server # -> To start the action server
+    go_to_pose_action_server_node = Node(
+        package="robot_patrol",
+        executable="go_to_pose_action_server_node",
+        output="screen",
+        emulate_tty=True
+    )
+
+    # Load RViz Configuration File #
+    rviz_config_file = "config.rviz"
+    rviz_config_path = os.path.join(package_directory, "rviz", rviz_config_file)
+    print("RViz Config Loaded !")
+
+    # Rviz2 Launch Configuration (RViz)#
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': False}], # Use the real system time
+        arguments=['-d', rviz_config_path]
+    )
+
+    return LaunchDescription([
+        go_to_pose_action_server_node,
+        rviz2_node
+    ])
